0f3aa8452a16d5d3b8c5895d2c06f1bd5d60f20e
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-24 11:08:14 +0600

    api integrate done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,14 +10,30 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List trendingMovies = [];
+  List topMovies = [];
+  List tv = [];
   final String apiKey = '808d181f4ea96902bfb2168fa14a6335';
   final readAccessToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDhkMTgxZjRlYTk2OTAyYmZiMjE2OGZhMTRhNjMzNSIsIm5iZiI6MTczMjQxNjM1Ny4wMzc3NjQ1LCJzdWIiOiI2NzQyOGQwZGFhYjcyMmJmYTdjOTAyOTIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.rcTbm5DP3vfgNn_I0zTTEIyQoMlKMylVQBl3DueRRSo';
 
+  @override
+  void initState() {
+    super.initState();
+    loadMovie();
+  }
+
   loadMovie() async {
     TMDB tmdbWithCustomLogs = TMDB(ApiKeys(apiKey, readAccessToken),
         logConfig: const ConfigLogger(showErrorLogs: true, showLogs: true));
-    Map trendingMovie = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map trendingResult = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map topMoviesResult = await tmdbWithCustomLogs.v3.movies.getTopRated();
+    Map tvMovieResult = await tmdbWithCustomLogs.v3.tv.getPopular();
+    setState(() {
+      trendingMovies = trendingResult['result'];
+      topMovies = topMoviesResult['result'];
+      tv = tvMovieResult['result'];
+    });
+    print(trendingMovies);
   }
 
   @override
