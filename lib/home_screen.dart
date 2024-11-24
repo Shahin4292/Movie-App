ce9f1b9097be5f90e4429c545bbb3738bc6f3ab7
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-24 10:01:29 +0600

    movie load and view function done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,6 +13,12 @@ class _HomeScreenState extends State<HomeScreen> {
   final String apiKey = '808d181f4ea96902bfb2168fa14a6335';
   final readAccessToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDhkMTgxZjRlYTk2OTAyYmZiMjE2OGZhMTRhNjMzNSIsIm5iZiI6MTczMjQxNjM1Ny4wMzc3NjQ1LCJzdWIiOiI2NzQyOGQwZGFhYjcyMmJmYTdjOTAyOTIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.rcTbm5DP3vfgNn_I0zTTEIyQoMlKMylVQBl3DueRRSo';
+
+  loadMovie() async {
+    TMDB tmdbWithCustomLogs = TMDB(ApiKeys(apiKey, readAccessToken),
+        logConfig: const ConfigLogger(showErrorLogs: true, showLogs: true));
+    Map trendingMovie = await tmdbWithCustomLogs.v3.trending.getTrending();
+  }
 
   @override
   Widget build(BuildContext context) {
