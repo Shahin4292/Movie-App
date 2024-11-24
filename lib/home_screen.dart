cb418a8c18db24833cf1731ba36bc7c7e7f269e1
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-24 09:49:32 +0600

    apiKey and AccessToken add done

---
+++ lib/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List trendingMovies = [];
+  final String apiKey = '808d181f4ea96902bfb2168fa14a6335';
+  final readAccessToken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDhkMTgxZjRlYTk2OTAyYmZiMjE2OGZhMTRhNjMzNSIsIm5iZiI6MTczMjQxNjM1Ny4wMzc3NjQ1LCJzdWIiOiI2NzQyOGQwZGFhYjcyMmJmYTdjOTAyOTIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.rcTbm5DP3vfgNn_I0zTTEIyQoMlKMylVQBl3DueRRSo';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "Movie App",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
